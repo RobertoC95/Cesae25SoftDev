b4c17e41c8537db935f9d5307cb7b90cada2dbe6
unknown <sw07@CESAEPRT.WAN>
2025-04-11 10:38:47 +0100

    segundo commit

---
+++ FichasPraticasJava/src/FichaPratica01/Ex01.java
@@ -0,0 +1,11 @@
+package FichaPratica01;
+
+public class Ex01 {
+
+    //criação scanner input utilizador
+    Scanner input = new Scanner(System.in);
+
+    // declarar variáveis
+    int numero1, numero2, soma;
+
+}
